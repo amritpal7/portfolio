4cb8f11828a9d83722d82bc376883c530a7844d4
amrit <mr.amrrit7@gmail.com>
2024-08-09 23:38:04 +0530

    Listed new project

--- src/data/projects_details.js
+++ src/data/projects_details.js
@@ -1,23 +1,23 @@
 export default [
-    {
-        completed: true,
-        title: "Todo List",
-        imgUrl: "/assets/a.jpg",
-        techStack: ['ReactJs', "Material Ui"],
-        link: "https://todo-ts-cyan.vercel.app/",
-    },
-    {
-        completed: false,
-        title: "",
-        imgUrl: "/assets/b.jpg",
-        techStack: ['','', ''],
-        link: "",
-    },
-    {
-        completed: false,
-        title: "",
-        imgUrl: "/assets/c.jpg",
-        techStack: ['','', ''],
-        link: "",
-    }
-]
+  {
+    completed: true,
+    title: "Todo List",
+    imgUrl: "/assets/a.jpg",
+    techStack: ["ReactJs", "Material Ui"],
+    link: "https://todo-ts-cyan.vercel.app/",
+  },
+  {
+    completed: true,
+    title: "Wikipedia mini",
+    imgUrl: "/assets/b.jpg",
+    techStack: ["NextJs", "Tailwind CSS"],
+    link: "https://wikirocket777.netlify.app",
+  },
+  {
+    completed: false,
+    title: "",
+    imgUrl: "/assets/c.jpg",
+    techStack: ["", "", ""],
+    link: "",
+  },
+];
