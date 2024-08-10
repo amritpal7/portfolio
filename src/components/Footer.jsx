4da0ecb5b295849e23e2fef9906745d80eaf8f2d
amrit <mr.amrrit7@gmail.com>
2024-08-10 20:34:03 +0530

    modify: minor footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
   return (
     <div className="py-5 text-center">
       <p className="text-sm mt-2 opacity-50">
-        &copy;{new Date().getFullYear()} Amritpal singh. All rigths reserved.
+        &copy;{new Date().getFullYear()} Amrit pal singh. All rigths reserved.
       </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
